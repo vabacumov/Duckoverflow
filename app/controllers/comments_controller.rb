7ed5ac5b767984bacf36177bdf9b025a7e4a9341
Chandler Smith & Evil Witch <pair+ChandlerHCSmith.kbasma@devbootcamp.com>
2014-08-31 15:19:32 -0700

    added new and create actions for the comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,37 @@
 class CommentsController < ApplicationController
   def new
-    @question = Question.find(params[:question_id])
+    if params[:question_id]
+      @question = Question.find(params[:question_id])
+    elsif params[:answer_id]
+      @answer = Answer.find(params[:answer_id])
+    end
   end
 
   def create
+    commentable_id = params[:question_id] ||= params[:answer_id]
+    if params[:answer_id]
+      commentable_type = "Answer"
+    else
+      commentable_type = "Question"
+    end
     @comment = Comment.new(content: params[:comment][:content],
                            user_id: session[:user_id],
-                           commentable_id: params[:question_id],
-                           commentable_type: "Question")
+                           commentable_id: commentable_id,
+                           commentable_type: commentable_type)
     if @comment.save
       user = User.find(session[:user_id])
       user.comments << @comment
-      question = Question.find(params[:question_id])
-      question.comments << @comment
-      redirect_to question_path(question.id)
+      if @comment.commentable_type == "Question"
+        question = Question.find(params[:question_id])
+        question.comments << @comment
+        redirect_to question_path(question.id)
+      elsif @comment.commentable_type == "Answer"
+        answer = Answer.find(params[:answer_id])
+        answer.comments << @comment
+        redirect_to question_path(answer.question.id)
+      end
     else
       render 'new'
     end
-
   end
 end
